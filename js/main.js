06764d486a1d74d799d0cbb810fe3d85070f6b63
caher15 <caherdev@gmail.com>
2026-01-19 21:08:17 -0600

    init repo

---
+++ js/main.js
@@ -0,0 +1,49 @@
+/*==================== SHOW NAVBAR ====================*/
+const showMenu = (headerToggle, navbarId) =>{
+    const toggleBtn = document.getElementById(headerToggle),
+    nav = document.getElementById(navbarId)
+    
+    // Validate that variables exist
+    if(headerToggle && navbarId){
+        toggleBtn.addEventListener('click', ()=>{
+            // We add the show-menu class to the div tag with the nav__menu class
+            nav.classList.toggle('show-menu')
+            // change icon
+            toggleBtn.classList.toggle('bx-x')
+        })
+    }
+}
+showMenu('header-toggle','navbar')
+
+/*==================== LINK ACTIVE ====================*/
+const linkColor = document.querySelectorAll('.nav__link')
+
+function colorLink(){
+    linkColor.forEach(l => l.classList.remove('active'))
+    this.classList.add('active')
+}
+
+linkColor.forEach(l => l.addEventListener('click', colorLink))
+
+
+
+/*==================== REVEAL PROJECT TITTLE ====================*/
+document.addEventListener("DOMContentLoaded", () => {
+  const reveals = document.querySelectorAll(".js-reveal");
+
+  const observer = new IntersectionObserver(
+    (entries, observer) => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add("is-visible");
+          observer.unobserve(entry.target); 
+        }
+      });
+    },
+    {
+      threshold: 0.3 
+    }
+  );
+
+  reveals.forEach(el => observer.observe(el));
+});
